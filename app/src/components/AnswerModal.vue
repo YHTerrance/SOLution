e48e324add157c3fc25a9240a957c3afbf1312db
YHTerrance <b08902073@ntu.edu.tw>
2022-04-25 10:40:22 +0800

    Fix ls

--- app/src/components/AnswerModal.vue
+++ app/src/components/AnswerModal.vue
@@ -8,11 +8,11 @@ import { submitAnswer } from "@/api";
 import { Status } from "@/models";
 
 function close_modal() {
-	if (!loading.value) emit("close");
+  if (!loading.value) emit("close");
 }
 const props = defineProps({
-	show: Boolean,
-	targetQuestion: Object,
+  show: Boolean,
+  targetQuestion: Object,
 });
 
 const { show, targetQuestion } = toRefs(props);
@@ -27,9 +27,9 @@ useAutoresizeTextarea(textarea);
 // Character limit / count-down.
 const characterLimit = useCountCharacterLimit(content, 280);
 const characterLimitColour = computed(() => {
-	if (characterLimit.value < 0) return "text-red-500";
-	if (characterLimit.value <= 10) return "text-yellow-500";
-	return "text-gray-400";
+  if (characterLimit.value < 0) return "text-red-500";
+  if (characterLimit.value <= 10) return "text-yellow-500";
+  return "text-gray-400";
 });
 
 // Permissions.
@@ -45,77 +45,71 @@ const ticks = ref(0); // Record the ticks passed during transaction confirmation
 const status = ref(new Status());
 
 const submit = async () => {
-	if (!canSubmit.value) return;
-	loading.value = true;
-	let answer;
-	try {
-		answer = await submitAnswer(
-			targetQuestion.value.publicKey,
-			content.value,
-			ticks
-		);
-		status.value.activate("success", "Successfully answered question!");
-		emit("added", answer);
-	} catch (error) {
-		console.log(error);
-		status.value.activate("danger", "Failed to answer");
-		emit("failed", answer);
-	} finally {
-		loading.value = false;
-		content.value = "";
-		console.log(ticks.value);
-		ticks.value = 0;
-		setTimeout(() => status.value.deactivate(), 5000);
-		close_modal();
-	}
+  if (!canSubmit.value) return;
+  loading.value = true;
+  let answer;
+  try {
+    answer = await submitAnswer(targetQuestion.value.publicKey, content.value, ticks);
+    status.value.activate("success", "Successfully answered question!");
+    emit("added", answer);
+  } catch (error) {
+    console.log(error);
+    status.value.activate("danger", "Failed to answer");
+    emit("failed", answer);
+  } finally {
+    loading.value = false;
+    content.value = "";
+    console.log(ticks.value);
+    ticks.value = 0;
+    setTimeout(() => status.value.deactivate(), 5000);
+    close_modal();
+  }
 };
 </script>
 
 <template>
-	<div
-		v-if="show"
-		class="fixed w-full h-full top-0 left-0 flex items-center justify-center"
-	>
-		<div
-			@click.self="close_modal()"
-			class="absolute w-full h-full bg-gray-800 opacity-50"
-		></div>
+  <div
+    v-if="show"
+    class="fixed w-full h-full top-0 left-0 flex items-center justify-center"
+  >
+    <div
+      @click.self="close_modal()"
+      class="absolute w-full h-full bg-gray-800 opacity-50 z-[-10]"
+    ></div>
 
-		<div
-			class="bg-white w-11/12 md:max-w-md mx-auto rounded-lg shadow-lg z-50 overflow-y-auto"
-		>
-			<div class="py-4 text-left px-6">
-				<div class="flex-col pb-3">
-					<h1 class="text-xl font-bold mt-2 mb-4">Answering...</h1>
-					<slot>Loading slot</slot>
-					<div v-if="connected" class="py-4 border-b">
-						<!-- Content field. -->
-						<textarea
-							ref="textarea"
-							rows="1"
-							class="text-xl p-0 w-full focus:outline-none resize-none mb-3 border-none focus:ring-transparent"
-							placeholder="What's your solution?"
-							v-model="content"
-						></textarea>
-					</div>
-					<div class="flex items-center space-x-6 m-2 ml-auto">
-						<!-- Character limit. -->
-						<div :class="characterLimitColour">
-							{{ characterLimit }} left
-						</div>
-						<!-- submit button. -->
-						<submit-button
-							:loading="loading"
-							:enabled="canSubmit"
-							:ticks="ticks"
-							@click="submit"
-						>
-							Answer
-						</submit-button>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<toast-item :status="status"></toast-item>
+    <div
+      class="bg-white w-11/12 md:max-w-md mx-auto rounded-lg shadow-lg overflow-y-auto z-100"
+    >
+      <div class="py-4 text-left px-6">
+        <div class="flex-col pb-3">
+          <h1 class="text-xl font-bold mt-2 mb-4">Answering...</h1>
+          <slot>Loading slot</slot>
+          <div v-if="connected" class="py-4 border-b">
+            <!-- Content field. -->
+            <textarea
+              ref="textarea"
+              rows="1"
+              class="text-xl p-0 w-full focus:outline-none resize-none mb-3 border-none focus:ring-transparent"
+              placeholder="What's your solution?"
+              v-model="content"
+            ></textarea>
+          </div>
+          <div class="flex items-center space-x-6 m-2 ml-auto">
+            <!-- Character limit. -->
+            <div :class="characterLimitColour">{{ characterLimit }} left</div>
+            <!-- submit button. -->
+            <submit-button
+              :loading="loading"
+              :enabled="canSubmit"
+              :ticks="ticks"
+              @click="submit"
+            >
+              Answer
+            </submit-button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <toast-item :status="status"></toast-item>
 </template>
--- app/src/components/QuestionBody.vue
+++ app/src/components/QuestionBody.vue
@@ -15,16 +15,14 @@ const emit = defineEmits(["edit"]);
 </script>
 
 <template>
-  <div class="relative">
+  <div>
     <h3 class="inline font-semibold" :title="question.author">
-      <!-- TODO: Link to author page or the profile page if it's our own question. -->
       <router-link :to="authorRoute" class="hover:underline">
         {{ question.author_display }}
       </router-link>
     </h3>
     <span class="text-gray-500"> • </span>
     <time class="text-gray-500 text-sm" :title="question.created_at">
-      <!-- TODO: Link to the question page. -->
       <router-link
         :to="{
           name: 'Question',
@@ -35,7 +33,7 @@ const emit = defineEmits(["edit"]);
         {{ question.created_at }}
       </router-link>
     </time>
-    <div class="inline-block absolute right-0 pt-1" v-if="isMyQuestion">
+    <div class="inline-block pt-1" v-if="isMyQuestion">
       <button
         @click="emit('edit')"
         class="flex px-2 rounded-full text-gray-500 hover:text-pink-500 hover:bg-gray-100"
