3cbe7b8c2422b5c723ee74f63e35fed5d6aae423
Chi-Feng Tsai <b07902123@ntu.edu.tw>
2022-05-25 09:35:42 +0000

    amend previous commit

--- app/src/api/index.js
+++ app/src/api/index.js
@@ -5,7 +5,5 @@ export * from "./fetch-answers";
 export * from "./submit-answer";
 export * from "./update-question";
 export * from "./update-answer";
-export * from "./delete-question";
-export * from "./delete-answer";
 export * from "./select-solution";
 export * from "./redeem-reward";
