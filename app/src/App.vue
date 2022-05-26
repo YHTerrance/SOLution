993973a4179f87a9b242f462e927628d017cb6c8
YHTerrance <b08902073@ntu.edu.tw>
2022-05-26 23:21:48 +0800

    Add chainlink pricefeed to useWorkspace.js composable

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,8 +2,8 @@
 import { useRoute } from "vue-router";
 import TheSidebar from "./components/TheSidebar";
 import {
-	PhantomWalletAdapter,
-	SolflareWalletAdapter,
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
 } from "@solana/wallet-adapter-wallets";
 import { initWallet } from "solana-wallets-vue";
 import { initWorkspace } from "./composables";
@@ -17,19 +17,19 @@ initWorkspace();
 </script>
 
 <template>
-	<div class="w-full max-w-3xl lg:max-w-4xl mx-auto">
-		<the-sidebar
-			class="py-4 md:py-8 md:pl-4 md:pr-8 fixed w-20 md:w-64"
-		></the-sidebar>
+  <div class="w-full max-w-3xl lg:max-w-4xl mx-auto">
+    <the-sidebar
+      class="py-4 md:py-8 md:pl-4 md:pr-8 fixed w-20 md:w-64"
+    ></the-sidebar>
 
-		<!-- Main -->
-		<main class="flex-1 border-r border-l ml-20 md:ml-64 min-h-screen">
-			<header
-				class="flex space-x-6 items-center justify-between px-8 py-4 border-b"
-			>
-				<div class="text-xl font-bold" v-text="route.name"></div>
-			</header>
-			<router-view></router-view>
-		</main>
-	</div>
+    <!-- Main -->
+    <main class="flex-1 border-r border-l ml-20 md:ml-64 min-h-screen">
+      <header
+        class="flex space-x-6 items-center justify-between px-8 py-4 border-b"
+      >
+        <div class="text-xl font-bold" v-text="route.name"></div>
+      </header>
+      <router-view></router-view>
+    </main>
+  </div>
 </template>
