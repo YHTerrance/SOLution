8159679b1e487cecd13e908f57514a73aed6b730
Chi-Feng Tsai <b07902123@ntu.edu.tw>
2022-05-26 14:52:20 +0000

    shows the calculated amount and make them unclickable

--- app/src/components/QuestionForm.vue
+++ app/src/components/QuestionForm.vue
@@ -138,8 +138,8 @@ const ask = async () => {
       </div>
 
       <!-- question button. -->
-      <div class="relative z-0 w-1/2 my-6 group">
-        <div class="inline-block w-5/12 p-2">
+      <div class="relative z-0 w-full my-6 group">
+        <div class="inline-block w-4/12 p-2">
           <label
             for="charge"
             class="peer-focus:font-medium absolute text-sm text-pink-500 dark:text-pink-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-pink-600 peer-focus:dark:text-pink-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
@@ -156,21 +156,29 @@ const ask = async () => {
             required
           />
         </div>
-        <span class="inline-block w-2/12 text-center text-lg">+</span>
-        <div class="inline-block w-5/12 p-2">
+        <span class="inline-block w-1/12 text-center text-lg">+</span>
+        <div class="inline-block w-3/12 p-2">
           <label
-            for="deposit"
             class="peer-focus:font-medium absolute text-sm text-pink-500 dark:text-pink-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-pink-600 peer-focus:dark:text-pink-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             Deposit (fixed)</label
           >
-          <input
-            type="text"
-            name="deposit"
-            :value="deposit"
+          <span
             class="block py-2.5 px-0 w-full text-sm text-pink-900 bg-transparent border-0 border-b-2 border-pink-300 appearance-none dark:text-white dark:border-pink-600 dark:focus:border-pink-500 focus:outline-none focus:ring-0 focus:border-pink-600 peer"
-            readonly
-          />
+          > {{ deposit }}
+          </span>
+        </div>
+        <span class="inline-block w-1/12 text-center text-lg">=</span>
+        <div class="inline-block w-3/12 p-2">
+          <label
+            class="peer-focus:font-medium absolute text-sm text-pink-500 dark:text-pink-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-pink-600 peer-focus:dark:text-pink-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Total</label
+          >
+          <span
+            class="block py-2.5 px-0 w-full text-sm text-pink-900 bg-transparent border-0 border-b-2 border-pink-300 appearance-none dark:text-white dark:border-pink-600 dark:focus:border-pink-500 focus:outline-none focus:ring-0 focus:border-pink-600 peer"
+          > {{ (amount ?? 0) + deposit }}
+          </span>
         </div>
       </div>
     </div>
