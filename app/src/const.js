b789b85a1ef81aff48720876a3d601a0e8a7928d
YHTerrance <b08902073@ntu.edu.tw>
2022-05-26 23:22:09 +0800

    Add fetch answers api author filter

--- app/src/const.js
+++ app/src/const.js
@@ -3,3 +3,14 @@ import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 const BASE_FEE_LAMPORTS = LAMPORTS_PER_SOL / 1000;
 
 export { BASE_FEE_LAMPORTS };
+
+export const FILTER = {
+  discriminator_length: 8,
+  public_key_length: 32,
+  timestamp_length: 8,
+  string_length_prefix: 4,
+  max_topic_length: 50 * 4,
+  max_content_length: 280 * 4,
+  amount_length: 8,
+  bool_length: 1,
+};
