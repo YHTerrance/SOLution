45812f6ed71f009d4fa1e4274dea71efb92e2ec8
YHTerrance <b08902073@ntu.edu.tw>
2022-06-02 09:57:37 +0800

    PageUsers.vue Fix single template element error for vuejs

--- app/src/components/PageUsers.vue
+++ app/src/components/PageUsers.vue
@@ -46,41 +46,43 @@ useFromRoute((route) => {
 </script>
 
 <template>
-  <question-search
-    placeholder="public key"
-    :disabled="!author"
-    v-model="author"
-    @search="search"
-  >
-    <template #icon>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-5 w-5"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z"
-        />
-      </svg>
-    </template>
-  </question-search>
-  <div v-if="viewedAuthor">
-    <question-list
-      v-model:questions="questions"
-      :loading="loading"
-      :has-more="hasNextPage"
-      @more="getNextPage"
-    ></question-list>
-    <div
-      v-if="!loading && questions.length === 0"
-      class="p-8 text-gray-500 text-center"
+  <div>
+    <question-search
+      placeholder="public key"
+      :disabled="!author"
+      v-model="author"
+      @search="search"
     >
-      User has not asked any questions...
+      <template #icon>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z"
+          />
+        </svg>
+      </template>
+    </question-search>
+    <div v-if="viewedAuthor">
+      <question-list
+        :questions="questions"
+        :loading="loading"
+        :has-more="hasNextPage"
+        @more="getNextPage"
+      ></question-list>
+      <div
+        v-if="!loading && questions.length === 0"
+        class="p-8 text-gray-500 text-center"
+      >
+        User has not asked any questions...
+      </div>
     </div>
   </div>
 </template>
