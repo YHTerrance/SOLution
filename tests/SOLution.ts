705818f6808d81b78333a3bd079f1acc51e6e961
YHTerrance <b08902073@csie.ntu.edu.tw>
2022-06-13 16:20:22 +0800

    Remove all reset functionality from test

--- tests/SOLution.ts
+++ tests/SOLution.ts
@@ -716,36 +716,4 @@ describe("SOLution", () => {
 			assert.equal(questionAccounts.length, 10);
 		});
 	});
-
-	describe("# Privileged", () => {
-		it("can delete all answers", async () => {
-			let answerAccounts = await program.account.answer.all();
-			for (const answerAccount of answerAccounts) {
-				await program.rpc.deleteAnswer({
-					accounts: {
-						answer: answerAccount.publicKey,
-						receiver: answerAccount.account.author,
-						authority: program.provider.wallet.publicKey,
-					},
-				});
-			}
-			answerAccounts = await program.account.answer.all();
-			assert.equal(answerAccounts.length, 0);
-		});
-
-		it("can delete all questions", async () => {
-			let questionAccounts = await program.account.question.all();
-			for (const questionAccount of questionAccounts) {
-				await program.rpc.deleteQuestion({
-					accounts: {
-						question: questionAccount.publicKey,
-						receiver: questionAccount.account.author,
-						authority: program.provider.wallet.publicKey,
-					},
-				});
-			}
-			questionAccounts = await program.account.question.all();
-			assert.equal(questionAccounts.length, 0);
-		});
-	});
 });
